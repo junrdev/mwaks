b7e25c57eb661fdabc89d533616eebc70c2d7260
star <juniorprogrammer09@gmail.com>
2023-12-03 09:34:57 +0300

    adapter update

--- app/src/main/java/ke/ac/mwaks/adapter/RecyclerItemWithRemoveOption.kt
+++ app/src/main/java/ke/ac/mwaks/adapter/RecyclerItemWithRemoveOption.kt
@@ -4,23 +4,41 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ke.ac.mwaks.R
 import ke.ac.mwaks.model.ListItemWithRemoveOption
+import ke.ac.mwaks.model.SearchItemCache
 
-class RecyclerItemWithRemoveOption(val items: MutableList<ListItemWithRemoveOption>) :
+class RecyclerItemWithRemoveOption(
+    val items: List<SearchItemCache>,
+    val onItemClickListener: (id: String) -> Unit
+) :
     RecyclerView.Adapter<RecyclerItemWithRemoveOption.RecyclerItemWithRemoveViewHolder>() {
 
     private val TAG = "RecyclerItemWithRemoveO"
+
+    class RecyclerItemWithRemoveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var itemName: TextView = itemView.findViewById(R.id.listItemText)
+        private var removeItemButton: ImageView = itemView.findViewById(R.id.listItemRemove)
+
+        fun bind(
+            searchItemCache: SearchItemCache,
+            onItemClickListener: (id: String) -> Unit
+        ) {
+            itemName.text = searchItemCache.text
+            removeItemButton.setOnClickListener { onItemClickListener(searchItemCache.id) }
+        }
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerItemWithRemoveViewHolder {
         val view =
-            LayoutInflater.from(parent.context.applicationContext).inflate(R.layout.recent_search_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.recent_search_item, parent, false)
         return RecyclerItemWithRemoveViewHolder(view)
     }
 
@@ -30,21 +48,7 @@ class RecyclerItemWithRemoveOption(val items: MutableList<ListItemWithRemoveOpti
 
     override fun onBindViewHolder(holder: RecyclerItemWithRemoveViewHolder, position: Int) {
         holder.bind(items[position]) {
-            Log.d(TAG, "onBindViewHolder: $position")
-            items.removeAt(position)
-        }
-    }
-
-    class RecyclerItemWithRemoveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private var itemName: TextView = itemView.findViewById(R.id.listItemText)
-        private var removeItemButton: ImageView = itemView.findViewById(R.id.listItemRemove)
-
-        fun bind(
-            listItemWithRemoveOption: ListItemWithRemoveOption,
-            callBack: (id: Int) -> Unit
-        ) {
-            itemName.text = listItemWithRemoveOption.listText
-            removeItemButton.setOnClickListener { callBack(listItemWithRemoveOption.id) }
+            id -> onItemClickListener(id)
         }
     }
 }
