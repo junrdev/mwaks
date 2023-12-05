0704d02fd512fc1898c9e09e5d5b1afc659ae182
star <juniorprogrammer09@gmail.com>
2023-12-05 16:01:03 +0300

    update

--- app/src/main/java/ke/ac/mwaks/fragments/SignUp.kt
+++ app/src/main/java/ke/ac/mwaks/fragments/SignUp.kt
@@ -1,11 +1,13 @@
 package ke.ac.mwaks.fragments
 
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.app.ProgressDialog
-import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -16,19 +18,26 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.UploadTask
+import com.google.firebase.storage.storage
 import ke.ac.mwaks.R
 import ke.ac.mwaks.model.AppUser
 import ke.ac.mwaks.util.FragmentButtonToActivityClickListener
 import ke.ac.mwaks.util.Methods
+import java.io.File
 import java.time.LocalDateTime
+
+private const val TAG = "SignUp"
 
 class SignUp : Fragment() {
 
-    private val TAG = "SignUp"
     private lateinit var msignUpSelectImage: ImageView
-    private lateinit var mselectedImage: ImageView
+    private lateinit var mselectedImage: ShapeableImageView
     private var auth = FirebaseAuth.getInstance()
 
     private lateinit var email: EditText
@@ -42,12 +51,11 @@ class SignUp : Fragment() {
 
     private lateinit var progress: ProgressDialog
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var imageUri: Uri
+    private val storageRef = Firebase.storage.reference
+    private val usersImagesRef = storageRef.child("userblob/profile")
+    private var filepath : String = "no_file_selected"
 
-        msignUpSelectImage = view.findViewById(R.id.signUpSelectImage)
-        msignUpSelectImage.setOnClickListener { Methods.openImagePicker(requireActivity()) }
-    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingInflatedId")
@@ -73,7 +81,9 @@ class SignUp : Fragment() {
         progress.setCancelable(false)
 
         msignUpSelectImage.setOnClickListener { v ->
-            Methods.openImagePicker(requireActivity())
+            openImage()
+
+
         }
 
         msignUpButton.setOnClickListener {
@@ -82,57 +92,17 @@ class SignUp : Fragment() {
             val pass = password.text.trim().toString()
 
             if (mail.isNotEmpty() && pass.isNotEmpty()) {
-                progress.show()
                 auth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener { it ->
                     if (it.isComplete) {
                         if (it.isSuccessful) {
                             val result = it.result
-                            val user = AppUser(
-                                result.user!!.uid,
-                                mail,
-                                "",
-                                LocalDateTime.now(),
-                                false,
-                                emptyList(),
-                                emptyList(),
-                                emptyList()
-                            )
-                            Log.d(TAG, "onCreateView: $result")
-
-                            // saving user metadata to database
-                            database.getReference("user_metadata").push().setValue(user)
-                                .addOnCompleteListener {
-
-                                    progress.hide()
-                                    if (it.isComplete) {
-                                        if (!it.isSuccessful) {
-                                            fragmentButtonToActivityClickListener.onButtonClicked()
-                                        } else {
-                                            Toast.makeText(
-                                                requireContext(),
-                                                "Failed to succeed with error : {it.exception!!.message}",
-                                                Toast.LENGTH_SHORT
-                                            ).show()
-                                        }
-                                    } else
-                                        Toast.makeText(
-                                            requireContext(),
-                                            "Failed to complete with error : ${it.exception!!.message}",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-
-                                    if (it.isCanceled)
-                                        Toast.makeText(
-                                            requireContext(),
-                                            "Cancelled with error : ${it.exception!!.message}",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-
-                                }
-                                .addOnFailureListener { e ->
-                                    Log.d(TAG, "onCreateView: Failed to save user : ${e.message}")
-                                }
-
+                            if (!filepath.equals("no_file_selected")) {
+                                val uploadFile = Uri.fromFile(File(filepath))
+                                val uploadTask = usersImagesRef.putFile(uploadFile)
+                                createUserWithProfilePic(uploadTask, result.user)
+                            } else {
+                                createUserWithoutProfilePic(result.user)
+                            }
                         } else {
                             auth.signOut()
                             Log.d(TAG, "onCreateView: ${it.exception!!.message}")
@@ -143,37 +113,155 @@ class SignUp : Fragment() {
             } else
                 Toast.makeText(requireContext(), "Provide all required fields.", Toast.LENGTH_SHORT)
                     .show()
+
         }
 
-        return view
-    }
+//        view.on
+    return view
+}
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == Methods.IMAGE_PICK_CODE && resultCode == RESULT_OK && data != null) {
-//            val selected = data.data
-//            Log.d(TAG, "onActivityResult: $data")
-//
-//            if (selected != null)
-//                Log.d(TAG, "onActivityResult: ${Methods.getFileName(selected, requireActivity())}")
-//            mselectedImage.setImageURI(selected)
-//
-//        }
-        Log.d(TAG, "onActivityResult: $resultCode  , data : $data")
-    }
+@RequiresApi(Build.VERSION_CODES.O)
+private fun createUserWithProfilePic(uploadTask: UploadTask, user: FirebaseUser?) {
+    uploadTask.continueWithTask {
+        if (!it.isSuccessful)
+            it.exception?.let {
+                throw it
+            }
+        usersImagesRef.downloadUrl
+    }.addOnCompleteListener {
+        if (it.isSuccessful) {
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is FragmentButtonToActivityClickListener)
-            fragmentButtonToActivityClickListener = context
-        else
-            throw RuntimeException("Please provide a implementation of FragmentButtonToActivityClickListener.")
-    }
+            val dl_url = it.result
 
+            usersImagesRef.metadata.addOnSuccessListener {
+                val user = AppUser(
+                    user!!.uid,
+                    email.text.toString(),
+                    dl_url.toString(),
+                    LocalDateTime.now(),
+                    true,
+                    emptyList(),
+                    emptyList(),
+                    emptyList()
+                )
+                // saving user metadata to database
+                database.getReference("user_metadata").push()
+                    .setValue(user)
+                    .addOnCompleteListener {
+
+//                            progress.hide()
+                        if (it.isComplete) {
+                            if (!it.isSuccessful) {
+                                fragmentButtonToActivityClickListener.onButtonClicked()
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Failed to succeed with error : {it.exception!!.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        } else
+                            Toast.makeText(
+                                requireContext(),
+                                "Failed to complete with error : ${it.exception!!.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        if (it.isCanceled)
+                            Toast.makeText(
+                                requireContext(),
+                                "Cancelled with error : ${it.exception!!.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                    }
+                    .addOnFailureListener { e ->
+                        Log.d(TAG, "onCreateView: Failed to save user : ${e.message}")
+                    }
+            }
+
+
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun createUserWithoutProfilePic(user: FirebaseUser?) {
+    val user = AppUser(
+        user!!.uid,
+        email.text.toString(),
+        Methods.ADMIN_IMAGE_URL,
+        LocalDateTime.now(),
+        true,
+        emptyList(),
+        emptyList(),
+        emptyList()
+    )
+    // saving user metadata to database
+    database.getReference("user_metadata").push()
+        .setValue(user)
+        .addOnCompleteListener {
+
+            progress.hide()
+            if (it.isComplete) {
+                if (!it.isSuccessful) {
+                    fragmentButtonToActivityClickListener.onButtonClicked()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed to succeed with error : {it.exception!!.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else
+                Toast.makeText(
+                    requireContext(),
+                    "Failed to complete with error : ${it.exception!!.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            if (it.isCanceled)
+                Toast.makeText(
+                    requireContext(),
+                    "Cancelled with error : ${it.exception!!.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+        }
+        .addOnFailureListener { e ->
+            Log.d(TAG, "onCreateView: Failed to save user : ${e.message}")
+        }
+
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+override fun onActivityResult(
+    requestCode: Int,
+    resultCode: Int,
+    data: Intent?
+) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == Methods.IMAGE_PICK_CODE && resultCode == RESULT_OK && data != null) {
+        data?.data?.let {
+            Log.d(TAG, "onActivityResult: $it")
+            imageUri = it
+            mselectedImage.setImageURI(it)
+            filepath = it.let {
+                requireContext()?.let { _context ->
+                    Methods.getPathFromUri(imageUri, _context)
+                }
+            }.toString()
+
+            Log.d(TAG, "onActivityResult: $filepath")
+
+        }
+    }
+}
+
+fun openImage() {
+    val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    startActivityForResult(intent, Methods.IMAGE_PICK_CODE)
+}
 
 }
