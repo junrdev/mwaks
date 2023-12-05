490817da97f8cd01010ce631ff6b575ead6a5b12
star <juniorprogrammer09@gmail.com>
2023-12-05 16:01:29 +0300

    update

--- app/src/main/java/ke/ac/mwaks/fragments/Uploads.kt
+++ app/src/main/java/ke/ac/mwaks/fragments/Uploads.kt
@@ -5,6 +5,8 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.camera2.CaptureRequest
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -20,18 +23,27 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ke.ac.mwaks.R
 import ke.ac.mwaks.adapter.RecyclerItemWithRemoveOption
+import ke.ac.mwaks.model.FileModel
 import ke.ac.mwaks.model.ListItemWithRemoveOption
+import ke.ac.mwaks.model.SearchItemCache
 import ke.ac.mwaks.util.Methods
+import java.io.File
 import java.security.Permissions
 
 private const val TAG = "Uploads"
+
 class Uploads : Fragment() {
 
     private lateinit var mselectedFilesRecycler: RecyclerView
     private lateinit var mopenCamera: CardView
     private lateinit var mfileUpload: CardView
     private lateinit var mimageUpload: CardView
+    private var selectedFiles = mutableListOf<SearchItemCache>()
+    private lateinit var adapter: RecyclerItemWithRemoveOption
+    private var fileList = mutableListOf<File>()
+    private lateinit var muploadButton: CardView
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,7 +54,7 @@ class Uploads : Fragment() {
         mopenCamera = view.findViewById(R.id.openCamera)
         mfileUpload = view.findViewById(R.id.fileUpload)
         mimageUpload = view.findViewById(R.id.imageUpload)
-
+        muploadButton = view.findViewById(R.id.uploadButton)
         mopenCamera.setOnClickListener {
 
             if (checkCameraPermissions() && checkFilePermissions())
@@ -74,23 +86,34 @@ class Uploads : Fragment() {
         }
         mfileUpload.setOnClickListener {
             if (checkFilePermissions())
-                Methods.openFilePicker(requireActivity())
+                openFilePicker()
         }
 
-        val adapter = RecyclerItemWithRemoveOption(
-            items = emptyList()
-        ){}
-        mselectedFilesRecycler.adapter = adapter
-        mselectedFilesRecycler.layoutManager = LinearLayoutManager(
-            requireActivity().applicationContext,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
 
+        adapter = RecyclerItemWithRemoveOption(
+            items = selectedFiles
+        ) {
+            Log.d(TAG, "onCreateView: $it")
+        }
+
+        mselectedFilesRecycler.adapter = adapter
+        mselectedFilesRecycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+
+        muploadButton.setOnClickListener {
+            Log.d(TAG, "onCreateView: ${fileList.toString()}")
+        }
         // Inflate the layout for this fragment
         return view
     }
 
+    fun openFilePicker() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "application/pdf"
+        startActivityForResult(intent, Methods.FILE_PICK_CODE)
+    }
 
     fun checkCameraPermissions(): Boolean {
         return ContextCompat.checkSelfPermission(
@@ -107,11 +130,20 @@ class Uploads : Fragment() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        Toast.makeText(requireContext(), "resultcode $resultCode", Toast.LENGTH_SHORT).show()
-        Log.d(TAG, "onActivityResult: req : $requestCode, data : $data")
 
+        Log.d(TAG, "onActivityResult: $resultCode data : $data")
+        if (requestCode == Methods.FILE_PICK_CODE && resultCode == Activity.RESULT_OK)
+            data?.data?.let {
+                val result = it
+                val fname = Methods.getFileName(result, requireActivity())
+                selectedFiles.add(SearchItemCache(text = fname))
+                adapter.notifyItemInserted(selectedFiles.size - 1)
+                if (it != null)
+                    fileList.add(File(Methods.getPathFromUri(it, requireContext())))
+            }
     }
 
     override fun onRequestPermissionsResult(
@@ -130,8 +162,7 @@ class Uploads : Fragment() {
                 Methods.openCamera(requireActivity());
         } else if (requestCode == Methods.FILES_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Methods.openFilePicker(requireActivity())
+                openFilePicker()
         }
     }
-
 }
